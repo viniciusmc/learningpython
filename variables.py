1bba1378b3655abcb6cc2561184b59ee814b286b
Vinicius Monteiro <vinicius.m.conceicao@gmail.com>
2017-03-13 00:16:09 -0300

    Second Lesson - Variables

---
+++ variables.py
@@ -0,0 +1,9 @@
+name = "Vinicius Monteiro"
+number = "1234"
+
+a = "A"
+_name = "Vinicius"
+camelCaseConvention = "camelCaseConvention"
+underscore_convention = "underscore_convention"
+
+# PEP8 - Python Conventions for Naming
