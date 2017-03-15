92125aa071bdd9195ec1dbf87fc4dd064fc5b24d
Vinicius Monteiro <vinicius.m.conceicao@gmail.com>
2017-03-15 01:51:46 -0300

    Second day of lessons

---
+++ string_functions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' String Functions'''
+
+SCIENCE = "SCIENCE"
+APPLE = "apples"
+
+print(APPLE)
+APPLE = APPLE.upper()
+print(APPLE)
+
+print(SCIENCE.lower())
+print(SCIENCE.title())
+
+APPLE = "             apple        "
+APPLE = APPLE.strip().upper()
+print(APPLE)
