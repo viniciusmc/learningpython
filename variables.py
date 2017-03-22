aee7b137a59800345697071616632604f762ba16
Vinicius Monteiro <vinicius.m.conceicao@gmail.com>
2017-03-22 06:26:38 -0300

    Removing unnecessary files

--- variables.py
+++
@@ -1,9 +0,0 @@
-name = "Vinicius Monteiro"
-number = "1234"
-
-a = "A"
-_name = "Vinicius"
-camelCaseConvention = "camelCaseConvention"
-underscore_convention = "underscore_convention"
-
-# PEP8 - Python Conventions for Naming
